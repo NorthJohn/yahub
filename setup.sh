c2d5f77921550d782f8a55cbcc379d162285ab86
NorthJohn <johnhackers@gmail.com>
2025-07-08 19:14:20 +0100

    tweaked to setup in home directory

--- setup.sh
+++ setup.sh
@@ -1,31 +1,39 @@
 #!/bin/bash
 #
+#
+# download this file into the home directory and run it
+#
+#   bash setup.sh
+#
 # this setup makes yahub run
-#  - from the pi account
+#  - from the users account
 #  - in a virtual environment
 #  - in the home directory
 #
-# the development and production are the same.
+# the development and production environment are one and the same.
 #
 
 
 echo "### update and upgrade ###"
 sudo apt update
-#sudo apt upgrade
+sudo apt upgrade
 
 echo "### install libraries ###"
 sudo apt install python3-pip mosquitto influxdb influxdb-client git
 
 echo "### create venv ###"
-cd /home/pi
+cd $HOME
 python3 -m venv py3
 source py3/bin/activate
 
 echo "### Installing python requirements ###"
 pip3 install pymodbus pyserial pyyaml paho-mqtt influxdb-client python-daemon
 
+echo "### Grab yahub and create yahub dir ###"
+git clone https://github.com/NorthJohn/yahub.git
+
 echo "### Creating config file ###"
-cd  /home/pi/yahub
+cd  $HOME/yahub
 cp  yahub.config.minimal.yaml yahub.config.yaml
 
 echo "### Get access to serial port ###"
@@ -39,9 +47,8 @@ echo "### Setting up systemd ###"
 sudo cp yahub.service /etc/systemd/system/
 sudo systemctl enable yahub
 
-#echo "### Setting up shutdown  ###"
+#echo "### Setting up shutdown before solar inverters shut the power off  ###"
 #sudo cp shutdown-at-dusk.* /etc/systemd/system/
 #sudo systemctl enable shutdown-at-dusk
-
 
 
